38109c1c58243e96bd3e4637c522f886009185ac
Cysan <53166358+18445@users.noreply.github.com>
2022-05-25 12:54:57 +0800

    Initial commit

---
+++ module_mine/src/main/java/com/jwzg/module_mine/MineMainActivity.kt
@@ -0,0 +1,11 @@
+package com.jwzg.module_mine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MineMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mine_activity_main)
+    }
+}
